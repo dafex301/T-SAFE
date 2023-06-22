db17af6f32753c411e3f917f26b06a5c2973ba65
Fahrel <fahrel@students.undip.ac.id>
2023-06-22 16:46:24 +0700

    feat: add laporan menu in admin

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -103,6 +103,18 @@
                                     clip-rule="evenodd" />
                             </svg>
                             Manajemen Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/history">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                style="width: 20px; margin: 0 5px 0 5px">
+                                <path fill-rule="evenodd"
+                                    d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zM9.75 17.25a.75.75 0 00-1.5 0V18a.75.75 0 001.5 0v-.75zm2.25-3a.75.75 0 01.75.75v3a.75.75 0 01-1.5 0v-3a.75.75 0 01.75-.75zm3.75-1.5a.75.75 0 00-1.5 0V18a.75.75 0 001.5 0v-5.25z"
+                                    clip-rule="evenodd" />
+                                <path
+                                    d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                            </svg>
+
+                            Manajemen Laporan</a></li>
                 @else
                     <li class="nav-item"><a class="nav-link" href="/lapor">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
